18232503dfe02f9aa6b6225ba98a5a2e2645f1d1
Sahil Latif <sahillatif072@gmail.com>
2024-04-13 16:50:22 +0500

    modified approach

--- LC_12th_April_Trapping_Rain_Water.cpp
+++ LC_12th_April_Trapping_Rain_Water.cpp
@@ -5,17 +5,16 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
-    int res = 0, n = height.size();  
-    for (int i = 1; i < n - 1; i++) { 
-        int left = height[i]; 
-        for (int j = 0; j < i; j++) 
-            left = max(left, height[j]);  
-        int right = height[i]; 
-        for (int j = i + 1; j < n; j++) 
-            right = max(right, height[j]); 
-        res = res + (min(left, right) - height[i]); 
-    } 
-  
+    int res = 0, n = height.size();
+    vector <int> left(n), right(n);
+    left[0] = height[0];
+    for(int i=1;i<n;i++)
+        left[i]=max(left[i-1],height[i]);
+    right[n-1] = height[n-1];
+    for(int i=n-2;i>=0;i--)
+        right[i]=max(right[i+1],height[i]);
+    for (int i = 1; i < n - 1; i++)
+        res += (min(left[i], right[i]) - height[i]); 
     return res; 
     }
 };
