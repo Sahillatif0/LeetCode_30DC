48bcbb9cfe764b2fe0f8c8533d82247b936f337c
Sahil Latif <sahillatif072@gmail.com>
2024-04-18 23:52:54 +0500

    Day 12 Med_Reverse_Integer

---
+++ D12_Med_Reverse_Integer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x != 0){
+            int digit = x % 10;
+            if(rev > INT_MAX/10 || rev < INT_MIN/10){
+                return 0;
+            }
+            rev = (rev * 10) + digit;
+            x = x/10;
+        }
+        return rev;
+    }
+};
+int main() {
+  Solution s;
+    cout<<s.reverse(123);  
+}
