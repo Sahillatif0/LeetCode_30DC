21158c83a3615d087b363f8633027437852f472d
Sahil Latif <sahillatif072@gmail.com>
2024-04-20 23:44:23 +0500

    LC_20th_April_Find_All_Groups_Of_Farmland

---
+++ LC_20th_April_Find_All_Groups_Of_Farmland.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+ public:
+  vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < land.size(); ++i)
+      for (int j = 0; j < land[0].size(); ++j)
+        if (land[i][j] == 1) {
+          int x = i;
+          int y = j;
+          dfs(land, i, j, x, y);
+          ans.push_back({i, j, x, y});
+        }
+
+    return ans;
+  }
+
+ private:
+  void dfs(vector<vector<int>>& land, int i, int j, int& x, int& y) {
+    if (i < 0 || i == land.size() || j < 0 || j == land[0].size())
+      return;
+    if (land[i][j] != 1)
+      return;
+    land[i][j] = 2;  // Mark as visited.
+    x = max(x, i);
+    y = max(y, j);
+    dfs(land, i + 1, j, x, y);
+    dfs(land, i, j + 1, x, y);
+  }
+};
+int main() {
+ Solution s;
+    vector<vector<int>> v{
+    {1,0,0},
+    {0,1,1},
+    {0,1,1}
+    };
+    auto ans = s.findFarmland(v);
+    for(auto i:ans){
+        for(auto j:i)
+            cout<<j<<" ";
+        cout<<endl;
+    }
+}
