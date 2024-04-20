4a84eb72e077772508efd38cb639ebb32eca139a
Sahil Latif <sahillatif072@gmail.com>
2024-04-21 01:10:44 +0500

    Day 16 Roman_To_Integer

---
+++ D16_Roman_To_Integer.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int ans = 0;
+        for(int i=0;i<s.size();i++){
+            if(i< s.size()-1 && m[s[i]]<m[s[i+1]])
+                ans-=m[s[i]];
+            else
+                ans+=m[s[i]];
+        }
+        return ans;
+    }
+};
+int main() {
+    Solution s;
+    cout<<s.romanToInt("MCMXCIV");
+}
